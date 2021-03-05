8e2789b3aac33b6c78525dff46d30003e7533975
Jenkins Automation Server <jenkins@ip-172-31-15-104.us-west-1.compute.internal>
2021-03-05 18:50:18 +0000

    second2

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class hello {
         public static void main(String[] args)
         {
                 int i;
-                for(i = 0; i <= 5; i++)
+                for(i = 0; i <= 10; i++)
                 {
                         System.out.println("Hello World .... "+i);
                 }
